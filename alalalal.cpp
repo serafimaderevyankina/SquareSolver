81d131a3f31d51fdc1df5a907b998e6f42aca98b
serafimaderevyankina <161410380+serafimaderevyankina@users.noreply.github.com>
2025-08-14 16:42:45 +0300

    Add files via upload
    штука для того, чтобы  заменять \t и \b на \\ при выводе

---
+++ alalalal.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+main ()
+{
+    int c;
+    c = 0;
+    while ((c = getchar()) != '\n')
+        if ((c = '\t') || (c = '\b'))
+        {
+            c = '\\';
+        }
+        putchar(c);
+    return 0;
+}
