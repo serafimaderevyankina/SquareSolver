cda2ba75e2eb42ebe123166b89027a8913c94be1
serafimaderevyankina <161410380+serafimaderevyankina@users.noreply.github.com>
2025-08-17 14:04:13 +0300

    Update 16.cpp
    я не знаю, можно ли так делать

--- 16.cpp
+++ 16.cpp
@@ -13,33 +13,11 @@ int main()
     for (i = 2; i < n + 1; i++)
     {
         g = pow(16, n - i - 2);
-        if (s[i] == 'A')
-        {
-            s[i] = 10;
-        }
-        if (s[i] == 'B')
-        {
-            s[i] = 11;
-        }
-        if (s[i] == 'C')
-        {
-            s[i] = 12;
-        }
-        if (s[i] == 'D')
-        {
-            s[i] = 13;
-        }
-        if (s[i] == 'E')
-        {
-            s[i] = 14;
-        }
-        if (s[i] == 'F')
-        {
-            s[i] = 15;
-        }
+       enum numbers {'A' = 10, 'B', 'C', 'D', 'F'};
         a = a + s[i]*g;
     }
     printf("%d", a);
     return 0;
 
 }
+
