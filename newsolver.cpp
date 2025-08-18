bfd67f7af28d999abb36f01855c49aa65b014503
serafimaderevyankina <161410380+serafimaderevyankina@users.noreply.github.com>
2025-08-18 18:38:59 +0300

    Update newsolver.cpp
    прототип+

--- newsolver.cpp
+++ newsolver.cpp
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <math.h>
+float a, b, c;
+char* solver(float a,float b,float c);
 int main(void)
 {
-    float a, b, c;
-    char s[];
-    printf("%f", a);
-    scanf("%f", a);
-    scanf("%f", b);
-    scanf("%f", c);
-    printf("%f, %f, %f\n", a, b, c);
-    s = solver(a, b, c)
-    printf("%s", s)
+    float as, bs, cs;
+    char s[2];
+    printf("%f", as);
+    scanf("%f", as);
+    scanf("%f", bs);
+    scanf("%f", cs);
+    printf("%f, %f, %f\n", as, bs, cs);
+    s = solver(as, bs, cs);
+    printf("%s", s);
 }
-char* solver(int a, b, c)
+char* solver( a, b, c)
 {
-    int a, b, c;
-    int a, b, c;
-    char s[];
-    d = x = y = 0;
-    int i = 0
+    float a, b, c;
+    char s[2];
+    float d, x, y = 0;
+    int i = 0;
     d = b*b - 4.0f*(a*c);
     /*printf("%f\n", d);*/
     if (a == 0)
