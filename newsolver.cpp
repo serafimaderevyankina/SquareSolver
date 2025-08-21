5c8786bb825d7c759f022e28522ab86b8303bd39
serafimaderevyankina <161410380+serafimaderevyankina@users.noreply.github.com>
2025-08-21 20:56:07 +0300

    Update newsolver.cpp
    переделала с указателями

--- newsolver.cpp
+++ newsolver.cpp
@@ -1,56 +1,51 @@
 #include <stdio.h>
 #include <math.h>
 float a, b, c;
-char* solver(float a,float b,float c);
+int solver( float *, float *);
 int main(void)
 {
-    float as, bs, cs;
-    char s[2];
-    printf("%f", as);
-    scanf("%f", as);
-    scanf("%f", bs);
-    scanf("%f", cs);
-    printf("%f, %f, %f\n", as, bs, cs);
-    s = solver(as, bs, cs);
-    printf("%s", s);
+    float x, y = 0;
+    float *px = &x;
+    float *py = &y;
+    solver(px, py);
+    printf("%f, %f", x,y);
+    return 0;
 }
-char* solver( a, b, c)
+int solver(float *px,float *py)
 {
-    float a, b, c;
-    char s[2];
-    float d, x, y = 0;
-    int i = 0;
+    float a, b, c, d;
+    scanf("%f", a);
+    scanf("%f", b);
+    scanf("%f", c);
+    printf("%f, %f, %f\n", a, b, c);
     d = b*b - 4.0f*(a*c);
     /*printf("%f\n", d);*/
     if (a == 0)
     {
         if (b == 0)
         {
-            return "no sense";
+            printf("no sense");
         }
         else
         {
-            x = (-c/b);
-            s[0] = x;
-            return s;
+            *px = (-c/b);
+            *py = (-c/b);
          }
     }
     else if ( a != 0 && d < 0)
     {
-           return "no sense";
+           printf( "no sense");
     }
     else if ( a != 0 && d > 0)
     {
-        x = ((-b - sqrt(d))/2.0f)/a;
-        y = ((-b + sqrt(d))/2.0f)/a;
-        s[0] = x;
-        s[1] = y;
-        return s;
+        *px = ((-b - sqrt(d))/2.0f)/a;
+        *py = ((-b + sqrt(d))/2.0f)/a;
+
     }
     else if (a != 0 && d == 0)
     {
-        x  = -b/ (2.0f*a);
-        s[0] = x;
-        return s;
+        *px  = -b/ (2.0f*a);
+        *py = -b/ (2.0f*a);
     }
+    return 0;
 }
