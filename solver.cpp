b92347dace9798841a8c6efc9c0393a0f99d218c
serafimaderevyankina <161410380+serafimaderevyankina@users.noreply.github.com>
2025-08-22 20:59:50 +0300

    Update solver.cpp
    добавила линейную функцию и функцию вывода

--- solver.cpp
+++ solver.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 float a, b, c;
-int solver( float, float, float, float *, float *);
+int quadsolver( float, float, float, float *, float *);
+int linesolver(float, float, float, float *, float *);
 int vvod(float *, float *, float *);
-int vyvod(int, int, float, float );
-int vyvod(int, int, float , float );
+int vyvod(int, int, int, float , float );
 int main(void)
  {
     printf("enter the coefficients of the quadratic equation\n");
@@ -16,36 +16,18 @@ int main(void)
     float *px = &x;
     float *py = &y;
     int ar = vvod(&a, pb, pc);
-    int n_roots = solver(a, b, c, px, py);
-    vyvod(ar, n_roots, x, y);
+    int n_roots = quadsolver(a, b, c, px, py);
+    int al = linesolver(a, b,  c, px, py);
+    vyvod(ar, n_roots, al, x, y);
     return 0;
 }
-int solver(float a, float b, float c, float *px,float *py)
+int quadsolver(float a, float b, float c, float *px,float *py)
 {
     float d = 0;
     /*printf("%f, %f, %f\n", a, b, c);*/
     d = b*b - 4.0f*(a*c);
     /*printf("%f\n", d);*/
-    if (a == 0)
-    {
-        if (b == 0)
-        {
-            if (c == 0)
-            {
-                return 3;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-        else
-        {
-            *px = (-c/b);
-            return 1;
-         }
-    }
-    else if ( a != 0 && d < 0)
+    if ( a != 0 && d < 0)
     {
        return 0;
     }
@@ -61,6 +43,36 @@ int solver(float a, float b, float c, float *px,float *py)
         *px  = -b/ (2.0f*a);
         return 1;
     }
+    else if (a == 0)
+    {
+        return 3;
+    }
+}
+int linesolver(float a, float b, float c, float *px, float *py)
+{
+    if (a == 0)
+    {
+        if (b == 0)
+        {
+            if (c == 0)
+            {
+                return 3;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            *px = -c/b;
+            return 1;
+        }
+    }
+    else
+    {
+        return 4;
+     }
 }
 int vvod(float *pa, float *pb, float *pc)
 {
@@ -83,30 +95,36 @@ int vvod(float *pa, float *pb, float *pc)
     }
 }
 
-int vyvod(int ar, int n_roots, float x, float y)
+int vyvod(int ar, int n_roots, int al, float x, float y)
 {
     if (ar == 0)
     {
-        if (n_roots == 0)
-        {
-            printf("no roots");
-        }
-        else if (n_roots == 3)
-        {
-            printf("infinity number of roots");
-        }
-        else if (n_roots == 2)
-        {
-            printf("solutions to the equation:%f, %f", x,y);
+        if (n_roots == 3)
+        {    if (al == 3)
+            {
+                printf("infinity number of roots");
+            }
+            else if (al == 0)
+            {
+                printf("no roots");
+            }
+            else if (al == 1)
+            {
+                printf("solutions to the equation:%f", x);
+            }
         }
         else if (n_roots == 1)
         {
             printf("solutions to the equation:%f", x);
         }
+        else if (n_roots == 2)
+        {
+          printf("solutions to the equation:%f, %f", x,y);
+        }
     }
     else
     {
-        printf("write numbers instead of words");
+       printf("write numbers instead of words");
     }
     return 0;
 }
